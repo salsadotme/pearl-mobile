1e5b118f55b13834222582a4d540c72a79890ee2
Zorayr Khalapyan <zkhalapyan@gmail.com>
2022-06-25 16:51:24 -0400

    Add cloud function for push tokens.

---
+++ cloud-functions/functions/src/deps/firebase.ts
@@ -0,0 +1,9 @@
+
+import * as admin from "firebase-admin";
+
+admin.initializeApp({});
+
+export {
+  admin,
+};
+
--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,9 +1,28 @@
+import {MessagingPayload} from "firebase-admin/lib/messaging/messaging-api";
 import * as functions from "firebase-functions";
+import {admin} from "./deps/firebase";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
+export interface PushPayload {
+  title: string;
+  content: string;
+  topic: string;
+}
+
+export const push = functions.runWith({
+  invoker: "public",
+}).https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+
+  const topic = "all";
+  const payload: PushPayload = {title: "Hello World", content: "foobar", topic: topic};
+
+  const message: MessagingPayload = {
+    notification: {
+      title: payload.title,
+      body: payload.content,
+      key: "1",
+    },
+  };
+  admin.messaging().sendToTopic(topic, message);
 });
