d29ee53ed5b1eb47bbec386d7cb84634815c209b
Zorayr Khalapyan <zkhalapyan@gmail.com>
2022-06-26 00:06:03 -0400

    Add photo to NFT projects screen.

--- ios-app/Shared/ProjectsView.swift
+++ ios-app/Shared/ProjectsView.swift
@@ -7,19 +7,20 @@
 
 import SwiftUI
 import UIKit
+import SDWebImageSwiftUI
 
 struct Project: Hashable {
     let id: String
     let title: String
     let content: String
-    let senderProfilePhoto: URL
+    let projectProfilePhoto: URL
 }
 
 let projects: [Project] = [
-    Project(id: "1", title: "Bullish Bears", content: "Yet another Messaging Protocol", senderProfilePhoto: URL(string: "https://pbs.twimg.com/profile_images/977496875887558661/L86xyLF4_400x400.jpg")!),
-    Project(id: "2", title: "Illuminati", content: "Yet another Messaging Protocol", senderProfilePhoto: URL(string: "https://pbs.twimg.com/profile_images/977496875887558661/L86xyLF4_400x400.jpg")!),
-    Project(id: "3", title: "Crypto Dates", content: "Yet another Messaging Protocol", senderProfilePhoto: URL(string: "https://pbs.twimg.com/profile_images/977496875887558661/L86xyLF4_400x400.jpg")!),
-    Project(id: "4", title: "FWB", content: "Yet another messaging protocol", senderProfilePhoto: URL(string: "https://pbs.twimg.com/profile_images/977496875887558661/L86xyLF4_400x400.jpg")!),
+    Project(id: "1", title: "Bullish Bears", content: "Yet another Messaging Protocol", projectProfilePhoto: URL(string: "https://pbs.twimg.com/profile_images/977496875887558661/L86xyLF4_400x400.jpg")!),
+    Project(id: "2", title: "Illuminati", content: "Yet another Messaging Protocol", projectProfilePhoto: URL(string: "https://pbs.twimg.com/profile_images/977496875887558661/L86xyLF4_400x400.jpg")!),
+    Project(id: "3", title: "Crypto Dates", content: "Yet another Messaging Protocol", projectProfilePhoto: URL(string: "https://pbs.twimg.com/profile_images/977496875887558661/L86xyLF4_400x400.jpg")!),
+    Project(id: "4", title: "FWB", content: "Yet another messaging protocol", projectProfilePhoto: URL(string: "https://pbs.twimg.com/profile_images/977496875887558661/L86xyLF4_400x400.jpg")!),
 ]
 
 struct ProjectsView: View {
@@ -33,47 +34,56 @@ struct ProjectsView: View {
     
     func cell(project: Project, isLast:Bool) -> some View {
         NavigationLink(destination: ToggleView()) {
-            VStack {
-                HStack{
+            VStack (alignment: .center, spacing: 0) {
+                HStack(alignment: .center) {
+                    WebImage(url: project.projectProfilePhoto)
+                        .resizable()
+                        .indicator(.activity)
+                        .animation(.easeInOut(duration: 0.5))
+                        .transition(.fade)
+                        .scaledToFill()
+                        .frame(width: 40.0, height: 40.0, alignment: .center)
+                        .clipShape(Circle())
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 15)
                     Text(project.title)
                     Spacer()
                     Image(systemName: "arrow.right")
-                }.padding(20)
+                        .padding()
+                }
                 if (!isLast) {
                     Divider()
                         .frame(height: 1.25)
                         .background(Color.white.opacity(0.25))
                 }
-            }
+            }.background(Color.white.opacity(0.10))
         }
     }
     
     var body: some View {
-        return NavigationView {
-            VStack (alignment: .leading){
-                Text("Select the projects you would like to receive notifications from.")
-                    .fixedSize(horizontal: false, vertical: true)
-                    .padding(.bottom, 15)
-                VStack(alignment: .leading, spacing: 0){
-                    ForEach(projects, id: \.self) { project in
-                        cell(project: project, isLast: project.id == "4")
-                    }
-                }
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10, style: .circular).stroke(Color.white.opacity(0.25), lineWidth: 1.25)
-                )
-                Spacer()
-                NavigationLink(destination: ContentView()) {
-                    Text("Continue")
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(RoundedRectangle(cornerRadius: 8).fill(Color(hex: 0x1890FF)))
+        VStack (alignment: .leading){
+            Text("Select the projects you would like to receive notifications from.")
+                .fixedSize(horizontal: false, vertical: true)
+                .padding(.bottom, 15)
+            VStack(alignment: .leading, spacing: 0){
+                ForEach(projects, id: \.self) { project in
+                    cell(project: project, isLast: project.id == "4")
                 }
             }
-            .padding()
-            .navigationTitle("Your NFTs")
-            .background(appBackgroundGradient)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10, style: .circular).stroke(Color.white.opacity(0.25), lineWidth: 1.25)
+            )
+            Spacer()
+            NavigationLink(destination: ContentView()) {
+                Text("Continue")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color(hex: 0x1890FF)))
+            }
         }
+        .padding()
+        .navigationTitle("Your NFTs")
+        .background(appBackgroundGradient)
         .foregroundColor(.white)
     }
 }
